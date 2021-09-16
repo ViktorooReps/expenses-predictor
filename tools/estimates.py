bb6ba681624fc367d36a0257810fe571dc49eb0b
BislanAshinov <mis-ter.ashinov@yandex.ru>
2021-09-17 02:00:52 +0300

    Add helpers.py and evaluate.py Don't check estimates.py

---
+++ tools/estimates.py
@@ -0,0 +1,13 @@
+from data.datamodel import Expenses
+
+Value = float
+
+
+class Estimates:
+    mae_in_category: Expenses
+    average_mae: Value
+    top_first_successes: Value
+    top_second_successes: Value
+    top_third_successes: Value
+    top_fourth_successes: Value
+    top_fifth_successes: Value
